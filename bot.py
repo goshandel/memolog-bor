cf89adbaf8eda9544ff8094066c674756e21d3be
Anastasiya <anpchernova@mail.ru>
2024-05-20 12:26:46 +0300

    base.py

--- bot.py
+++ bot.py
@@ -2,5 +2,11 @@
 # мв в начале кода проходили как
 # из комманд только start
 # примеры паролей я могу скинуть
+import telebot as telebot
 from config import token
 
+bot = telebot.TeleBot(token=token)
+
+@bot.message_handler(commands=["start"])
+def challenge_to_duel(message):
+    bot.send_message(message.chat.id, "привет")
