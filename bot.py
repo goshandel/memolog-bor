e77f9ee08122c87d9b13458f0937abd94f2c9776
Anastasiya <anpchernova@mail.ru>
2024-05-20 21:19:57 +0300

    base.py

--- bot.py
+++ bot.py
@@ -4,9 +4,13 @@
 # примеры паролей я могу скинуть
 import telebot as telebot
 from config import token
+from database import Users_base
 
 bot = telebot.TeleBot(token=token)
 
 @bot.message_handler(commands=["start"])
 def challenge_to_duel(message):
+    base = Users_base()
+    if not base.check_user_exists(message.chat.id):
+        base.add_user(message.chat.id, message.chat.first_name, message.chat.username)
     bot.send_message(message.chat.id, "привет")
