e885e7689da843aa032a3997cbf8cd6b624961ff
Anastasiya <anpchernova@mail.ru>
2024-05-25 23:41:05 +0300

    base.py

--- buttons.py
+++ buttons.py
@@ -47,5 +47,43 @@ choice_month.add(December, January, February, March, April, May, June, July, Aug
 month_list = ["12", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11"]
 
 add_meme1 = InlineKeyboardMarkup(row_width=1)
-add_meme1.add(InlineKeyboardButton("год🎈", callback_data="year"),
-             InlineKeyboardButton("месяц🌝", callback_data="month"))
+add_meme1.add(InlineKeyboardButton("год🎈", callback_data="year_for_admin"),
+             InlineKeyboardButton("месяц🌝", callback_data="month_for_admin"))
+
+choice_year_for_admin = InlineKeyboardMarkup(row_width=5)
+y_2024_for_admin = InlineKeyboardButton("2024", callback_data="02024")
+y_2023_for_admin = InlineKeyboardButton("2023", callback_data="02023")
+y_2022_for_admin = InlineKeyboardButton("2022", callback_data="02022")
+y_2021_for_admin = InlineKeyboardButton("2021", callback_data="02021")
+y_2020_for_admin = InlineKeyboardButton("2020", callback_data="02020")
+y_2019_for_admin = InlineKeyboardButton("2019", callback_data="02019")
+y_2018_for_admin = InlineKeyboardButton("2018", callback_data="02018")
+y_2017_for_admin = InlineKeyboardButton("2017", callback_data="02017")
+y_2016_for_admin = InlineKeyboardButton("2016", callback_data="02016")
+y_2015_for_admin = InlineKeyboardButton("2015", callback_data="02015")
+y_2014_for_admin = InlineKeyboardButton("2014", callback_data="02014")
+y_2013_for_admin = InlineKeyboardButton("2013", callback_data="02013")
+y_2012_for_admin = InlineKeyboardButton("2012", callback_data="02012")
+choice_year_for_admin.add(y_2024_for_admin, y_2023_for_admin, y_2022_for_admin, y_2021_for_admin, y_2020_for_admin)
+choice_year_for_admin.add(y_2019_for_admin, y_2018_for_admin, y_2017_for_admin)
+choice_year_for_admin.add(y_2016_for_admin, y_2015_for_admin, y_2013_for_admin, y_2012_for_admin)
+year_list_for_admin = ["02024", "02023", "02022", "02021", "02020", "02019", "02018", "02017", "02016", "02015", "02014", "02013", "02012"]
+
+choice_month_for_admin = InlineKeyboardMarkup(row_width=3)
+December_for_admin = InlineKeyboardButton("ДЕК", callback_data="012")
+January_for_admin = InlineKeyboardButton("ЯНВ", callback_data="001")
+February_for_admin = InlineKeyboardButton("ФЕВ", callback_data="002")
+March_for_admin = InlineKeyboardButton("МАРТ", callback_data="003")
+April_for_admin = InlineKeyboardButton("АПР", callback_data="004")
+May_for_admin = InlineKeyboardButton("МАЙ", callback_data="005")
+June_for_admin = InlineKeyboardButton("ИЮНЬ", callback_data="006")
+July_for_admin = InlineKeyboardButton("ИЮЛЬ", callback_data="007")
+August_for_admin = InlineKeyboardButton("АВГ", callback_data="008")
+September_for_admin = InlineKeyboardButton("СЕНТ", callback_data="009")
+October_for_admin = InlineKeyboardButton("ОКТ", callback_data="010")
+November_for_admin = InlineKeyboardButton("НОЯ", callback_data="011")
+choice_month_for_admin.add(December_for_admin, January_for_admin, February_for_admin,
+                           March_for_admin, April_for_admin, May_for_admin,
+                           June_for_admin, July_for_admin, August_for_admin,
+                           September_for_admin, October_for_admin, November_for_admin)
+month_list_for_admin = ["012", "001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011"]
